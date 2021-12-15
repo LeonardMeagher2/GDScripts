9fe5d1ed550be513f8ddc9df9e5971f5d0ea4a30
LeonardMeagher2 <leonardmeagher2@gmail.com>
2021-12-14 19:25:11 -0800

    accidently created a cyclic dependency

--- Scripts/UAI/BehaviorContext.gd
+++ Scripts/UAI/BehaviorContext.gd
@@ -1,15 +1,8 @@
 extends Reference
 class_name UAIBehaviorContext
 
-signal completed
-
 var behavior:Resource
 var behavior_score:Reference
 var target_ref:WeakRef
 var agent_ref:WeakRef
 var current_time:Resource
-
-func execute_action() -> UAIAction.ActionExecution:
-	var execution = behavior.action.execute(self)
-	execution.connect("completed", self, "emit_signal", ["completed"], CONNECT_ONESHOT)
-	return execution
