f331d058a26230beaf3f07b3e6b015d1bd629686
LeonardMeagher2 <leonardmeagher2@gmail.com>
2021-12-15 17:39:10 -0800

    clamp consideration weights to 0-1 and lerp to 1 based on inverse weight

--- Scripts/UAI/Behavior.gd
+++ Scripts/UAI/Behavior.gd
@@ -3,10 +3,11 @@ extends Resource
 class_name UAIBehavior
 
 export(int, "Normal", "High") var priority:int = 0
-export(Array, Resource) var preconditions:Array
-export(Array, Resource) var considerations:Array setget set_considerations
 export var weight:float = 1.0
 export var action:Resource
+export(Array, Resource) var preconditions:Array
+export(Array, Resource) var considerations:Array setget set_considerations
+
 
 var consideration_weights = {}
 
@@ -23,10 +24,8 @@ func _get(property):
 	return null
 
 func _set(property,value):
-	print(property)
 	if property.begins_with("consideration_weights/"):
 		var name = property.split("/",false,1)
-		print(name)
 		if name.size() == 2:
 			consideration_weights[name[1]] = value
 			return true
@@ -63,17 +62,19 @@ func score(context:UAIBehaviorContext) -> Score:
 		scores.final_score = weight
 		return scores
 	
-	var compensation = 1.0 - (1.0 / considerations.size())
-	var result = weight
-	for consideration_weight in consideration_weights.values():
-		result += consideration_weight
+	var compensation:float = 1.0 - (1.0 / considerations.size())
+	var result:float = weight
 	
 	for consideration in considerations:
+		# a consideration weight of 0 will not contribute at all to the final score
+		var consideration_weight:float = consideration_weights[consideration.name]
+		
 		var score:UAIConsideration.Score = consideration.score(context)
 		var modification = (1.0 - score.final_score) * compensation
 		score.final_score = score.final_score + (modification * score.final_score)
 		
-		result *= score.final_score
+
+		result *= lerp(score.final_score, 1.0, 1.0-consideration_weight)
 		scores.considerations[consideration] = score
 		
 	scores.final_score = result
@@ -90,7 +91,8 @@ func _get_property_list():
 		res.append({
 			name = "consideration_weights/"+consideration.name,
 			type = TYPE_REAL,
-			hint_string = "consideration_weight_" + consideration.name,
+			hint = PROPERTY_HINT_RANGE,
+			hint_string = "0,1,0.01,true,consideration_weight_" + consideration.name,
 			usage = PROPERTY_USAGE_DEFAULT
 		})
 	return res
